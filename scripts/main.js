d14e6d6ff26b1b7f23a40de97e2d9160d46907d0
merinobakker <70895319+merinobakker@users.noreply.github.com>
2020-11-02 22:03:12 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+const menuButton = document.querySelector('.menu-button');
+let menuOpen = false;
+menuButton.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuButton.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuButton.classList.remove('open');
+        menuOpen = false;
+    }
+});
